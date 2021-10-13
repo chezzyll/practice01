281b61e2db036a50160e992c6a7af4304cd63e63
Vladislav Untura <chezzyll.vu@gmail.com>
2021-10-14 02:03:12 +0300

    add const personalMovieDB

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 "use strict";
 
 const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
